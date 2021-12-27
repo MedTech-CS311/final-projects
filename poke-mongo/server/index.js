862e7dd24e2e0a102628e48e267ab655008e1934
eya26 <eya.chafter@medtech.tn>
2021-12-27 22:32:28 +0100

    1st

--- poke-mongo/server/index.js
+++ poke-mongo/server/index.js
@@ -1,3 +1,16 @@
-/**
- * Your server comes here
- */
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+app.get("/api/pokemon",(req,res) => {
+    res.status(200).send("Fetching...")
+})
+
+app.post("api/pokemon", (req,res) => {
+    console.log(req.body)
+    res.status(201).send("Creating user ...")
+})
+
+app.listen(8000, () => {
+    console.log("started listening for resquests on port 8000");
+})
