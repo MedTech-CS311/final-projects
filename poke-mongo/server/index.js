4b84fccc4db38a85c0ab94d9dfedaf4fd621ad93
chaimarajah <chaima.rajah@medtech.tn>
2021-12-27 21:58:44 +0100

    my first commit

--- poke-mongo/server/index.js
+++ poke-mongo/server/index.js
@@ -1,3 +1,8 @@
-/**
- * Your server comes here
- */
+const express = require ('express')
+const app = express ()
+const port = 8000
+app.listen (port , ()=> {
+    console.log('listening in port 8000') 
+
+})
+
