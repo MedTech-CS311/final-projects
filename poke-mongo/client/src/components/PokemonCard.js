2a568aa65538f9b7fcb0745998e5046bf0480596
Mohamed Aziz Maazouz <aziz.maazouz@medtech.tn>
2021-12-29 23:07:26 +0100

    Cards now open official wiki-link in a new tab on click

--- poke-mongo/client/src/components/PokemonCard.js
+++ poke-mongo/client/src/components/PokemonCard.js
@@ -1,5 +1,9 @@
 function PokemonCard(props)
 {   
+    const openWikiLink = () =>{
+            const link = 'https://www.pokemon.com/us/pokedex/'+props.name;
+            window.open(link, '_blank').focus();
+    }
     var t = "";
     var id = "";
     if(props.types.length > 1)
@@ -15,7 +19,7 @@ function PokemonCard(props)
 
     return (
         
-        <div className= "card" id={id} >
+        <div className= "card" id={id} onClick = {openWikiLink} >
             <h2>{props.name}</h2>
             <img width="200" height="200" src={props.imageUrl} alt={props.name + "'s image is loading.."}></img>
             <div className= "cardDescription">
