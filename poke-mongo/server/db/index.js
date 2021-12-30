d8b297a871f58cc3e91111ef9a16804ddc56044d
rayan589 <rayan.belaam@gmail.com>
2021-12-30 13:59:10 +0100

    backend

--- poke-mongo/server/db/index.js
+++ poke-mongo/server/db/index.js
@@ -1,3 +1,6 @@
-/**
- * Your Database Connection comes here
- */
+const mongoose = require ('mongoose');
+
+
+mongoose.connect('mongodb+srv://rayan:azerty123@cluster0.f8ili.mongodb.net/Pokemon?retryWrites=true&w=majority');
+
+module.exports = router;
