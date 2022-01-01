23a9c1981ac24925af96b9dbbf3c30a0d9d2fff3
rayan589 <rayan.belaam@gmail.com>
2022-01-01 11:21:40 +0100

    Final Version

--- poke-mongo/poke-mongo/server/resources/pokemon/pokemonRouter.js
+++ poke-mongo/poke-mongo/server/resources/pokemon/pokemonRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();  // to manage routes
 
-const pokemonController = require('/Users/macbookpro/Documents/GitHub/final-projects/poke-mongo/server/resources/pokemon/pokemonController.js');
-const Pokemon = require('/Users/macbookpro/Documents/GitHub/final-projects/poke-mongo/server/resources/pokemon/pokemon.js');
+const pokemonController = require('./pokemonController.js');
+const Pokemon = require('./pokemon.js');
 
 
 router.get('/', pokemonController.get_pokemon);
-router.post('/', pokemonController.post_pokemon);
+router.post('/pokemon', pokemonController.post_pokemon);
 router.delete('/', pokemonController.delete_pokemon);
 router.get('/:number', pokemonController.get_number_pokemon);
 router.put('/:number', pokemonController.update_number_pokemon);
--- poke-mongo/server/db/index.js
+++ poke-mongo/server/db/index.js
@@ -1,6 +1,3 @@
-const mongoose = require ('mongoose');
-
-
-mongoose.connect('mongodb+srv://rayan:azerty123@cluster0.f8ili.mongodb.net/Pokemon?retryWrites=true&w=majority');
-
-module.exports = router;
+/**
+ * Your Database Connection comes here
+ */
